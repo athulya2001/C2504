634b492cfb924f2566c5ae91c0cb6a34104354cb
athulya2001 <72291114+athulya2001@users.noreply.github.com>
2024-08-14 19:51:41 +0530

    Add files via upload

---
+++ 14-08-24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Threading.Tasks;
+
+
+using ProgramingFundamentalsProject.TrainerApp;
+namespace ProgramingFundamentalsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrainerMenu.Menu();
+            Console.ReadKey();
+        }
+
+
+    }
+}
